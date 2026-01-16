77a1b0bab22c7fcd5971f83fd01bb244d6aa42c2
Dexter <dextercaron04@gmail.com>
2026-01-16 22:44:53 +0800

    Fix vite.config.js base path for localhost development

--- source-code/frontend/vite.config.js
+++ source-code/frontend/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
+  base: process.env.VITE_BASE_PATH || "/",
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
